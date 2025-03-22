062c46b821d076b7225b84c35430403bcc4a3b46
Motz <mjlane88@gmail.com>
2025-03-22 12:56:55 -0500

    Updated JS with TodoCounter class

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,15 @@ import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
+import TodoCounter from "../components/TodoCounter.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopupEl = document.querySelector("#add-todo-popup");
 const addTodoForm = addTodoPopupEl.querySelector(".popup__form");
 const addTodoCloseBtn = addTodoPopupEl.querySelector(".popup__close");
 const todosList = document.querySelector(".todos__list");
+
+const todoCounter = new TodoCounter(initialTodos, ".counter__text");
 
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
@@ -39,10 +42,6 @@ const section = new Section({
 
 addTodoPopup.setEventListeners();
 
-const closeModal = (modal) => {
-  modal.classList.remove("popup_visible");
-};
-
 const generateTodo = (data) => {
   const todo = new Todo(data, "#todo-template");
   const todoElement = todo.getView();
@@ -53,22 +52,6 @@ addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
   newTodoValidator.resetValidation();
 });
-
-/*addTodoForm.addEventListener("submit", (evt) => {
-  evt.preventDefault();
-  const name = evt.target.name.value;
-  const dateInput = evt.target.date.value;
-
-  const date = new Date(dateInput);
-  date.setMinutes(date.getMinutes() + date.getTimezoneOffset());
-
-  const id = uuidv4();
-  const values = { name, date, id };
-  const todo = generateTodo(values);
-  section.addItem(todo);
-  newTodoValidator.resetValidation();
-  addTodoPopup.close();
-});*/
 
 initialTodos.forEach((item) => {
   const todo = generateTodo(item);
